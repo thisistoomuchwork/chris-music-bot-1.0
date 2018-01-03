e3364874fd4bca0d239c15422ed867ed9f45e229
thisistoomuchwork <35054924+thisistoomuchwork@users.noreply.github.com>
2018-01-03 11:22:18 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.py');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
